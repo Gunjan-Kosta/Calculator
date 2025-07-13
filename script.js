72039e0823e855498dac9791d22508b5f88603d5
GUNJAN KOSTA <gunjan.kosta2023@ssipmt.com>
2025-07-13 21:23:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,6 @@ function clearDisplay() {
   document.getElementById("display").value = "";
 }
 
-function dlt() {
-  document.getElementById("display").value -= 1;
-}
 function calculate() {
   let result = eval(document.getElementById("display").value);
   document.getElementById("display").value = result;
